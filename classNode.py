29efe93e7077a1598256946920decad1673cdf4a
108ym <117145813+108ym@users.noreply.github.com>
2023-07-06 22:36:10 +0800

    classNode.py

--- classNode.py
+++ classNode.py
@@ -1,7 +1,6 @@
 class Node:
-    def __init__(self, x,y, bin_size = 0, bin_weight = 0, rubbish_weight= 0, rubbish_size = 0, disposal_room=0, parent=None, action=None):
-        self.x = x #x coordinate
-        self.y = y #y coordinate
+    def __init__(self, q,r,s, bin_size = 0, bin_weight = 0, rubbish_weight= 0, rubbish_size = 0, disposal_room=0, parent=None, action=None):
+        self.cube = (q,r,s) #coordinate
         self.bin_size = bin_size #0-5
         self.bin_weight = bin_weight #0-40
         self.rubbish_weight =  rubbish_weight #0/5/10/20/30

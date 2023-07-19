769cd5823d63f77360d53517712a761fb3a97a2f
Fololorose <105091673+Fololorose@users.noreply.github.com>
2023-07-20 05:39:09 +0800

    Update maze_clearing.py

--- maze_clearing.py
+++ maze_clearing.py
@@ -1,17 +1,14 @@
 from math import floor
 import numpy as np
-import heapq
 
 class Node:
-  def __init__(self, x, y, rubbish_size = 0, rubbish_weight= 0, disposal_room=0, parent=None, action=None, f_score=0):
+  def __init__(self, x, y, rubbish_size = 0, rubbish_weight = 0, disposal_room = 0, h_score = 0):
     self.coordinate = (x, y) # Axial coordinate
     self.rubbish_size = rubbish_size # 1/2/3
     self.rubbish_weight =  rubbish_weight # 0/5/10/20/30
     self.disposal_room = disposal_room # True/False 
-    # self.parent = parent
-    # self.action = action
     self.children = []
-    self.f_score = f_score
+    self.h_score = h_score
 
   def add_children(self, children):
     self.children.extend(children)
@@ -47,46 +44,64 @@ def achieve_goal(current_node):
     return False
 
 # Determine possible child nodes to become frontier
-def expand_and_return_children(current_node, directions):
+def expand_and_return_children(state_space, node, directions, current_bin_size, current_bin_weight):
   children = []
   for direction in directions.values():
-    new_coordinate = tuple(np.add(np.array(current_node.coordinate),np.array(direction)))
-    if new_coordinate in state_space:
-      child = state_space[new_coordinate]
-      children.append(child)
+    child = tuple(np.add(np.array(node.coordinate),np.array(direction)))
+    for [x, y] in state_space:
+      if child[0] == x and child[1] == y:
+        children.append(Node(child[0], child[1]))
+        children[-1].h_score = heuristics(state_space, children[-1], current_bin_size, current_bin_weight)
   return children
 
 # Find the estimation cost to the closest disposal room or room with rubbish
-def closest_target_room(child, target_rooms):
+def closest_target_room(child, target_rooms, current_bin_size, current_bin_weight):
   direct_distance = []
   temp_child_coordinate = child.coordinate + (z_coordinate(child.coordinate[0], child.coordinate[1]),)
   for target_room in target_rooms:
-    coordinates_substract = []
-    temp_target_room_coordinate = target_room.coordinate + (z_coordinate(target_room.coordinate[0], target_room.coordinate[1]),)
-    for i in range(0, 3):
-      coordinates_substract.append(abs(temp_child_coordinate[i] - temp_target_room_coordinate[i]))
-    direct_distance.append(max(coordinates_substract))
+    if ((current_bin_size + target_room.rubbish_size) <= max_bin_size) and ((current_bin_weight + target_room.rubbish_weight) <= max_bin_weight): 
+      coordinates_substract = []
+      temp_target_room_coordinate = target_room.coordinate + (z_coordinate(target_room.coordinate[0], target_room.coordinate[1]),)
+      for i in range(0, 3):
+        coordinates_substract.append(abs(temp_child_coordinate[i] - temp_target_room_coordinate[i]))
+      direct_distance.append(max(coordinates_substract))
+    else:
+      # If the bin size or bin weight exceeds the maximum, set direct_distance to a large value to discourage selecting this target
+      direct_distance.append(float("inf"))
   return min(direct_distance)
 
 # Heuristics euclidean distance
-def heuristics(child, state_space):
+def heuristics(state_space, child, current_bin_size, current_bin_weight):
   # Retrieve the nodes with disposal room and the nodes with rubbish
   disposal_rooms = [node for node in state_space.values() if node.disposal_room]
   rooms_with_rubbish = [node for node in state_space.values() if node.rubbish_size != 0 or node.rubbish_weight != 0]
   # Probably gonna implement weightage
-  h_score = closest_target_room(child, disposal_rooms) + closest_target_room(child, rooms_with_rubbish) 
-  return h_score
+  heuristic1 = closest_target_room(child, rooms_with_rubbish, current_bin_size, current_bin_weight)
+  heuristic2 = closest_target_room(child, disposal_rooms, current_bin_size, current_bin_weight)
+  return min(heuristic1, heuristic2)
+
 
 # Just for testing
-def append_and_sort(current_node, frontier, child, g_score, f_score, came_from): 
-  if child not in [i[1] for i in frontier]:
-      came_from[child] = current_node
-      g_score[child] = g_score[current_node] + 1
-      f_score[child] = g_score[child] + heuristics(child, state_space)
-      heapq.heappush(frontier, ((f_score[child], child.coordinate), child))
+def append_and_sort(frontier, child): 
+  duplicated = False
+  removed = False
+  for i, f in enumerate(frontier):
+    if child.coordinate == f.coordinate:
+      duplicated = True
+      if child.h_score <= f.h_score:
+        del frontier[i]
+        removed = True
+        break
+  if (not duplicated) or removed:
+    insert_index = len(frontier)
+    for i, f in enumerate(frontier):
+      if child.h_score <= f.h_score:
+        insert_index = i
+        break
+    frontier.insert(insert_index, child)
   return frontier
 
-def a_star(initial_node):
+def greedy(state_space, rooms_with_rubbish, disposal_rooms, initial_node):
   # Direction (only store manipulation of x and y)
   directions = {
     "N": (0, +1),
@@ -98,24 +113,35 @@ def a_star(initial_node):
   }
     
   goal_achieved = False
-  frontier = []
   explored = []
-  came_from = {}
-  g_score = {initial_node: 0}
-  f_score = {initial_node: heuristics(initial_node, state_space)}
+  frontier = []
+  solution = []
+  indices_to_remove = []
+  current_bin_size = 0
+  current_bin_weight = 0
     
   # Add initial node to frontier
-  # frontier.append(initial_node)
-  heapq.heappush(frontier, (f_score[initial_node], initial_node))
-    
+  frontier.append(Node(initial_node[0], initial_node[1]))
+  
   while not goal_achieved:
-    current_node = heapq.heappop(frontier)[1]
-    # current_node = frontier[0]
-    for room_with_rubbish in rooms_with_rubbish:
+    current_node = state_space[frontier[0].coordinate]
+    for i, room_with_rubbish in enumerate(rooms_with_rubbish):
       if current_node.coordinate == room_with_rubbish[0]:
+        current_bin_size += current_node.rubbish_size
+        current_bin_weight += current_node.rubbish_weight
         current_node.rubbish_size = 0
         current_node.rubbish_weight = 0
-      rooms_with_rubbish.remove(room_with_rubbish)
+        indices_to_remove.append(i)
+
+      # Remove the elements from rooms_with_rubbish using the collected indices
+      for index in reversed(indices_to_remove):
+        rooms_with_rubbish.pop(index)
+        indices_to_remove.clear()
+    
+    for i, disposal_room in enumerate(disposal_rooms):
+      if current_node.coordinate == disposal_room:
+        current_bin_size = 0
+        current_bin_weight = 0
         
     # Goal test at expansion
     if achieve_goal(current_node):
@@ -123,28 +149,31 @@ def a_star(initial_node):
       break
     
     # Expand the first in the frontier
-    children = expand_and_return_children(current_node, directions)
+    children = expand_and_return_children(state_space, frontier[0], directions, current_bin_size, current_bin_weight)
     # Add children list to the expanded node
-    current_node.add_children(children)
+    frontier[0].add_children(children)
     # Add to the expanded node explored list
-    explored.append(current_node)
-    # # Remove the expanded frontier from the frontier list
-    # del frontier[0]
+    explored.append(frontier[0])
+    # Remove the expanded frontier from the frontier list
+    current_expanded_node = frontier.pop(0)
+    # Add the expanded node to the solution (optimal path)
+    solution.append(current_expanded_node.coordinate)
+    print(*solution)
     # Add children to the frontier
     for child in children:
-      # Check if a node was expanded or generated previously
-      if not (child.coordinate in [e.coordinate for e in explored]):       
-        frontier = append_and_sort(current_node, frontier, child, g_score, f_score, came_from)
+      # Check if a node was expanded or generated previously      
+      if not(child.coordinate in [e.coordinate for e in explored]):
+        frontier = append_and_sort(frontier, child)
         
     print("Explored:", [e.coordinate for e in explored])
-    print("Frontier:", [(f.coordinate, f_score[f]) for _, f in frontier])
+    print("Frontier:", [(f.coordinate, f.h_score) for f in frontier])
     print("Children:", [c.coordinate for c in children])
     print("")
     
-  solution = []
-  while current_node in came_from:   
-    solution.append(current_node)
-    current_node = came_from[current_node]
+    next_expanded_node = frontier.pop(0)
+    frontier.clear()
+    frontier.append(next_expanded_node)
+    
   return solution
 
 # Define state space (maze size, rubbish, disposal room)
@@ -160,7 +189,6 @@ if __name__ == "__main__":
   # Determine disposal rooms, rooms with rubbish including rubbish size & weight
   disposal_rooms = [(2, -6), (5, -2), (8, -9)] # (x,y)
   rooms_with_rubbish = [ #(x, y, rubbish_size, rubbish weight)
-      ((0, 0), 1, 30),
       ((0, -5), 1, 10),
       ((1, -3), 3, 30),
       ((2, -3), 1, 5),
@@ -170,28 +198,19 @@ if __name__ == "__main__":
       ((4, -6), 1, 20),
       ((6, -4), 2, 10),
       ((6, -7), 2, 5),
+      ((7, -3), 1, 30),
       ((7, -6), 2, 20),
       ((8, -5), 3, 10)
   ]
     
   generate_maze(maze_height, maze_width, state_space) 
     
-  initial_node = state_space[(0, 0)]
+  initial_node = (0, 0)
     
-  
-    
-  # # Print the state_space
-  # for coordinate, node in state_space.items():
-  #     print("Coordinate:", coordinate)
-  #     print("Rubbish Size:", node.rubbish_size)
-  #     print("Rubbish Weight:", node.rubbish_weight)
-  #     print("Disposal Room:", node.disposal_room)
-  #     print("---")
-  
-  solution  = a_star(initial_node)
-  solution_coordinates = [node.coordinate for node in solution]
-  print("Solution:", solution_coordinates)
-  # print("Path Cost:", cost)
+  solution = greedy(state_space, rooms_with_rubbish, disposal_rooms, initial_node)
+  print("Solution:", solution)
+#   greedy(state_space, rooms_with_rubbish, disposal_rooms, initial_node)
+#   print("Path Cost:", cost)
     
     
     
